a2e61810fd9a6d84ed85372a8ab84298c63a07c3
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2021-11-13 11:59:20 -0700

    Migration: add article foreign key to comments table

--- priv/repo/migrations/20211113075428_create_comments_table.exs
+++ priv/repo/migrations/20211113075428_create_comments_table.exs
@@ -3,9 +3,10 @@ defmodule ConduitElixir.Repo.Migrations.CreateCommentsTable do
   @timestamps_opts [type: :utc_datetime_usec, usec: true]
 
   def change do
-    create table(:comments) do 
+    create table(:article_comments) do 
       add :body, :string, null: false
       add :user_id, references(:users, on_delete: :delete_all)
+      add :article_id, references(:articles, on_delete: :delete_all)
 
       timestamps(@timestamps_opts)
     end
